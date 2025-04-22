4ebad6555dcdab122827c06c5234499b16fa1539
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2025-04-22 07:06:31 +0200

    add html boilerplates for courses, fixed index footer links

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,12 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'), // Entrada principal
         legal: resolve(__dirname, 'pages/legal-advise.html'), // Aviso legal
         who: resolve(__dirname, 'pages/quienes-somos.html'), // Quienes somos
+        cursoFullstack: resolve(__dirname, 'pages/cursos-fullstack.html'), //Curso1
+        cursoBlockchain: resolve(__dirname, 'pages/cursos-blockchain.html'), //Curso2
+        cursoCibersegruidad: resolve(__dirname, 'pages/cursos-ciberseguridad.html'), //Curso3
+        cursos: resolve(__dirname, 'pages/cursos.html'), //Cursos
         // blog: resolve(__dirname, 'pages/blog.html'), //Blog
         // contact: resolve(__dirname, 'pages/contact.html'), // Contact
-        // curso1: resolve(__dirname, 'pages/curso1.html'), //Curso1
-        // curso2: resolve(__dirname, 'pages/curso2.html'), //Curso2
-        // cursos: resolve(__dirname, 'pages/cursos.html'), //Cursos
         // login: resolve(__dirname, 'pages/login.html'), //Login
         // noti1: resolve(__dirname, 'pages/noticia1.html'), //Noticia1
         // noti2: resolve(__dirname, 'pages/noticia2.html'), //Noticia2
