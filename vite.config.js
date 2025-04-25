b501d9fa3655b322303bb9e8dc24e8fc6e3c9386
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2025-04-25 05:22:19 +0200

    linked all the pages

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,12 @@ export default defineConfig({
         cursoCibersegruidad: resolve(__dirname, 'pages/cursos-ciberseguridad.html'), //Curso3
         cursos: resolve(__dirname, 'pages/cursos.html'), //Cursos
         blog: resolve(__dirname, 'pages/blog.html'), //Blog
+        new_algoritmo: resolve(__dirname, 'pages/new_algoritmo.html'), //Noticia 1
+        new_bit: resolve(__dirname, 'pages/new_bit.html'), //Noticia 2
+        new_diagrama: resolve(__dirname, 'pages/new_diagrama.html'), //Noticia 3
+        new_junior: resolve(__dirname, 'pages/new_junior.html'), //Noticia 4
+        new_lenguaje: resolve(__dirname, 'pages/new_lenguaje.html'), //Noticia 5
+        new_marketer: resolve(__dirname, 'pages/new_marketer.html'), //Noticia 6
         // contact: resolve(__dirname, 'pages/contact.html'), // Contact
         // login: resolve(__dirname, 'pages/login.html'), //Login
         // noti1: resolve(__dirname, 'pages/noticia1.html'), //Noticia1
